0084f816bd7dda4b1d6953202a7afdab6ed50b67
Finoana Idealy <154631122+Finoanaa@users.noreply.github.com>
2025-06-11 08:09:01 +0300

    feat: add HelloWorldController with /hello endpoint

---
+++ src/main/java/com/finoana/arith/endpoint/rest/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.finoana.endpoint.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "... world!";
+  }
+}
