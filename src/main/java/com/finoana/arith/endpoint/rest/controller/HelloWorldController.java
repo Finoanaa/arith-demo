23b39e2d57bb2e8cb0b65245a25c86e846db4968
Finoana Idealy <154631122+Finoanaa@users.noreply.github.com>
2025-06-22 22:11:44 +0300

    Update HelloWorldController.java

--- src/main/java/com/finoana/arith/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/finoana/arith/endpoint/rest/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.finoana.endpoint.rest.controller;
+package com.finoana.arith.endpoint.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return "... world!";
-    }
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "... world!";
+  }
 }
